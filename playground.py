58ec7f648db912723763c41e0d4c22ab064a9b8d
Rylan <rylanschaeffer@gmail.com>
2019-10-28 17:06:45 -0400

    Implementation of Equation 12 doesn't seem to train successfully. Need to reevaluate

--- playground.py
+++ playground.py
@@ -5,7 +5,7 @@ import numpy as np
 # analogous to original synthetic dataset. See:
 # https://github.com/KaosEngineer/PriorNetworks-OLD/blob/master/prior_networks/dirichlet/dirichlet_prior_network_synth.py#L73
 
-num_points = 301
+num_points = 501
 # ensure num_points is divisible by 3
 num_points = 3 * (num_points // 3)
 mixture_proportions = [1 / 3.0, 1 / 3.0, 1 / 3.0]
@@ -69,15 +69,16 @@ class Network(nn.Module):
             out_features=3)
 
     def forward(self, x):
-        x = self.weights(x)
-        return x
+        output = F.softmax(self.weights(x), dim=1)
+        return output
 
 
 net = Network()
 
 
 # define typical classification function
-criterion = nn.CrossEntropyLoss()
+# criterion = nn.CrossEntropyLoss()
+criterion = nn.NLLLoss()
 
 
 # train model
@@ -86,7 +87,7 @@ import torch.optim as optim
 
 optimizer = optim.SGD(net.parameters(), lr=0.01)
 num_training_steps = 100
-batch_size = 10
+batch_size = 20
 
 # convert data to tensors
 samples = torch.Tensor(samples)
@@ -94,6 +95,7 @@ samples = torch.Tensor(samples)
 labels = torch.Tensor(labels).to(torch.int64)
 
 # train!
+losses = []
 for step in range(num_training_steps):
     optimizer.zero_grad()   # zero the gradient buffers
     batch_idx = np.random.choice(
@@ -104,8 +106,66 @@ for step in range(num_training_steps):
     pred_labels = net(batch_samples)
     loss = criterion(pred_labels, batch_labels)
     print('Step: {}, Loss: {}'.format(step, loss.item()))
+    losses.append(loss.item())
     loss.backward()
     optimizer.step()
 
 
-# define alternative loss function
+# plot training loss
+import plotly.graph_objects as go
+
+
+fig = go.Figure(go.Scatter(
+    x=np.arange(len(losses)),
+    y=losses,
+    mode='lines'))
+fig.show()
+
+# define softened labels
+one_hot_labels = F.one_hot(labels).to(torch.float32)
+epsilon = 0.01
+soft_labels = one_hot_labels - one_hot_labels*len(mu)*epsilon + epsilon
+
+
+# define loss function
+# first term of equation 12. See
+# https://github.com/KaosEngineer/PriorNetworks-OLD/blob/master/prior_networks/dirichlet/dirichlet_prior_network_synth.py#L107
+from torch.distributions import Dirichlet
+from torch.distributions.kl import kl_divergence
+
+
+def eqn_twelve(model_softmax_outputs, soft_labels):
+    target_dirichlet = Dirichlet(soft_labels)
+    model_dirichlet = Dirichlet(model_softmax_outputs)
+    kl_div = kl_divergence(target_dirichlet, model_dirichlet)
+    return torch.mean(kl_div)
+
+
+# train!
+losses = []
+for step in range(num_training_steps):
+    optimizer.zero_grad()   # zero the gradient buffers
+    batch_idx = np.random.choice(
+        np.arange(num_points),
+        size=batch_size,
+        replace=False)
+    batch_samples, batch_labels = samples[batch_idx], soft_labels[batch_idx]
+    pred_labels = net(batch_samples)
+    loss = eqn_twelve(
+        model_softmax_outputs=pred_labels,
+        soft_labels=batch_labels)
+    print('Step: {}, Loss: {}'.format(step, loss.item()))
+    losses.append(loss.item())
+    loss.backward()
+    optimizer.step()
+
+
+# plot training loss
+import plotly.graph_objects as go
+
+
+fig = go.Figure(go.Scatter(
+    x=np.arange(len(losses)),
+    y=losses,
+    mode='lines'))
+fig.show()
